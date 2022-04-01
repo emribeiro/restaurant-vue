82ac659ffbf36396729f5653685d7846a6613782
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2022-04-01 19:56:05 -0300

    Cart Mobile Icon

--- src/composable/composable.js
+++ src/composable/composable.js
@@ -3,3 +3,8 @@ export function isDesktop(){
     const width = window.innerWidth;
     return width > 767;
 };
+
+export function isSmallScreen(){
+    const width = window.innerWidth;
+    return width < 767;
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <div class="cart-icon" v-if="isSmallScreen">
+            <CartIcon />
+        </div>
         <CategoryMenu />
         <ItemsList />
         <Cart />
@@ -11,14 +14,22 @@
 import CategoryMenu from "../components/CategoryMenu.vue"
 import ItemsList from "../components/ItemsList.vue"
 import Cart from "../components/Cart.vue"
+import CartIcon from "../assets/icons/cart.svg";
+import { isSmallScreen } from "../composable/composable.js";
 
 export default {
     name: 'Home', 
     components: {
-    CategoryMenu,
-    ItemsList,
-    Cart
-}
+        CategoryMenu,
+        ItemsList,
+        Cart,
+        CartIcon
+    },
+    data(){
+        return{
+            isSmallScreen: isSmallScreen()
+        }
+    }
 }
 </script>
 
@@ -30,6 +41,21 @@ export default {
 
         @media @tablets{
             flex-direction: column;
+        }
+
+        .cart-icon{
+            width: 42px;
+            height: 42px;
+            background: @pink;
+            display: flex;
+            border-radius: 50%;
+
+
+            justify-content: center;
+            align-items: center;
+
+            margin: 20px 20px 20px auto;
+
         }
     }
 </style>
